d6b902990b3f3f94c3d645346b7c259c49ece07f
nicolae <nicolaeturcan@iesebre.com>
2015-01-14 21:34:32 +0100

    Some changes

--- src/com/example/prova/MainActivity.java
+++ src/com/example/prova/MainActivity.java
@@ -2,9 +2,6 @@ package com.example.prova;
 
 import java.io.InputStream;
 import java.net.URL;
-
-import com.iesebre.dam2.pa201415.nicolae.botons.R;
-
 import android.support.v7.app.ActionBarActivity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -14,16 +11,41 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
-	ImageView mImageView;
+	private TextView lblMissatge;
+
+	private ImageView mImageView;
+	private Button btnBoto1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		btnBoton1 = (Button)findViewById(R.id.BtnBoto);
+
+		btnBoto1 = (Button) findViewById(R.id.BtnBoto);
+
+		btnBoto1.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				new Thread(new Runnable() {
+					public void run() {
+
+						lblMissatge.setText("Botó Clicat!");
+
+						final Bitmap bitmap = loadImageFromNetwork("http://2.bp.blogspot.com/-yvPsukTegU4/UyMOA_dsuhI/AAAAAAAAKPA/5K7ZDjKwIMA/s1600/bridge_png_by_evelivesey-d5psbyl.png");
+						View mImageView = null;
+						mImageView.post(new Runnable() {
+							public void run() {
+								Object mImageView = null;
+								((ImageView) mImageView).setImageBitmap(bitmap);
+							}
+						});
+					}
+				}).start();
+			}
+
+		});
 
 	}
 
@@ -44,23 +66,6 @@ public class MainActivity extends ActionBarActivity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
-	}
-
-	public void addListenerOnButton() {
-	public void onClick(View v) {
-	    new Thread(new Runnable() {
-	        public void run() {
-	            final Bitmap bitmap = loadImageFromNetwork("http://2.bp.blogspot.com/-yvPsukTegU4/UyMOA_dsuhI/AAAAAAAAKPA/5K7ZDjKwIMA/s1600/bridge_png_by_evelivesey-d5psbyl.png");
-	            View mImageView = null;
-				mImageView.post(new Runnable() {
-	                public void run() {
-	                    Object mImageView = null;
-						((ImageView) mImageView).setImageBitmap(bitmap);
-	                }
-	            });
-	        }
-	    }).start();
-	}
 	}
 
 	private Bitmap loadImageFromNetwork(String url) {
